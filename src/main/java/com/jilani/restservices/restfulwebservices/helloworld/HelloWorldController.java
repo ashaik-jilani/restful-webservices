a48ddcca279090f258eb20bae30a2b3a0a1dc3b2
ashaik-jilani <ashaik.jilani@gmail.com>
2019-08-15 22:11:26 +0400

    Updated with i18n feature

--- src/main/java/com/jilani/restservices/restfulwebservices/helloworld/HelloWorldController.java
+++ src/main/java/com/jilani/restservices/restfulwebservices/helloworld/HelloWorldController.java
@@ -3,8 +3,13 @@
  */
 package com.jilani.restservices.restfulwebservices.helloworld;
 
+import java.util.Locale;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -13,6 +18,9 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 public class HelloWorldController {
+	
+	@Autowired
+	private MessageSource messageSource;
 	
 	@GetMapping(path="hello-world")
 	public String helloWorld() {
@@ -26,7 +34,13 @@ public class HelloWorldController {
 	
 	@GetMapping(path="hello-world/{name}")
 	public HelloWorldBean helloWorldPath(@PathVariable String name) {
-		return new HelloWorldBean(String.format("Hello Wolrd!, %s", name));
+		return new HelloWorldBean(String.format("Hello World!, %s", name));
+	}
+	
+	
+	@GetMapping(path="/hello-i18n")
+	public String helloWorldI18n(@RequestHeader(name="Accept-Language",required = false) Locale locale) {
+		return  messageSource.getMessage("good.morning.message", null, locale);
 	}
 	
 	
